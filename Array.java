38186d407bec25ba16ad8757293b772c99e98079
Raghu0027 <rs3638441@gmail.com>
2025-10-08 00:46:43 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,12 @@
+public class Array {
+    public static void main(String[] arg){
+        int[] arr={1,2,3,4,5,6,7,8,9};
+        for(int i=0; i<10; i++){
+            if(arr[i]%2!=0){
+                System.out.println(arr[i]);
+            }
+        }
+        
+    }
+    
+}
